b298f6755b4bd09decce6217dca8e31d014adb51
Sankalp Gulati <sankalp.gulati@upf.edu>
2016-09-25 20:09:23 +0200

    added plot functions

--- plotUtils/PhDPlottingCode.py
+++ plotUtils/PhDPlottingCode.py
@@ -9,6 +9,9 @@ import copy
 
 import matplotlib as mpl
 
+#mpl.rc('font',family='Times New Roman')
+mpl.rcParams['text.usetex'] = True
+mpl.rcParams['text.latex.unicode'] = True
 mpl.rc('font',family='Times New Roman')
 
 
@@ -199,3 +202,61 @@ def plotMotifPairs(motifFile):
         plotPairs(pitchData[start_ind1:end_ind1,1],  pitchData[start_ind2:end_ind2,1])
 
 
+def plotGamakaExample(pitchFile, plotName=-1, tonic = 146.8325, time_range= []):
+
+	#Hindustani
+	#Example 1: plt.plotGamakaExample('GamakaExamples/hindustani/Bhimpalasi_S-m-g_Stereo.pitch', plotName='GamakaExamples/hindustani/Bhimpalasi_S-m-g_Stereo.pdf',  tonic = 146.8325)
+	#Example 2: plt.plotGamakaExample('GamakaExamples/hindustani/Bageshri_S-m-g_Stereo.pitch', plotName='GamakaExamples/hindustani/Bageshri_S-m-g_Stereo.pdf',  tonic = 146.8325)
+	#Example 3: plt.plotGamakaExample('GamakaExamples/hindustani/DemoAlankar.pitch', plotName='GamakaExamples/hindustani/DemoAlankar.pdf',  tonic = 146.8325)
+	#Example 4: plt.plotGamakaExample('GamakaExamples/hindustani/DemoAlankar.pitch', plotName='GamakaExamples/hindustani/DemoAlankar_2.7__11.6.pdf',  tonic = 146.8325, time_range=[2.7, 11.6])
+	#Example 5: plt.plotGamakaExample('GamakaExamples/hindustani/DemoAlankar.pitch', plotName='GamakaExamples/hindustani/DemoAlankar_13.5__18.pdf',  tonic = 146.8325, time_range=[13.5, 18])
+
+	#Carnatic1: 
+	#Example 1: plt.plotGamakaExample('GamakaExamples/carnatic/Sphuritam_on_M1_Raga_Arabhi.pitch', plotName='GamakaExamples/carnatic/Sphuritam_on_M1_Raga_Arabhi.pdf', tonic = 146.8325)
+	#Example 2: plt.plotGamakaExample('GamakaExamples/carnatic/Kampitam_on_N2_Todi.pitch', plotName='GamakaExamples/carnatic/Kampitam_on_N2_Todi.pdf', tonic = 146.8325)
+	#Example 3: plt.plotGamakaExample('GamakaExamples/carnatic/Kampitam_on_N2_Rag_Ahiri.pitch', plotName='GamakaExamples/carnatic/Kampitam_on_N2_Rag_Ahiri.pdf', tonic = 146.8325)
+	#Example 4: plt.plotGamakaExample('GamakaExamples/carnatic/Odukkal_on_D1.pitch', plotName='GamakaExamples/carnatic/Odukkal_on_D1.pdf', tonic = 146.8325)
+
+	pitchData = np.loadtxt(pitchFile)
+	ind_non_zero = np.where(pitchData[:,1]>60)[0]
+	ind_zero = np.where(pitchData[:,1]<=60)[0]
+	pitchData[ind_non_zero,1] = 1200*np.log2(pitchData[ind_non_zero,1]/tonic)
+	pitchData[ind_zero,1] = -10000
+	hop = pitchData[1,0]-pitchData[0,0]
+
+	if len(time_range) == 2:
+		start_ind1 = np.argmin(abs(pitchData[:,0]-time_range[0]))	
+		end_ind1 = np.argmin(abs(pitchData[:,0]-time_range[1]))
+	else:
+		start_ind1 = 0
+		end_ind1 = pitchData.shape[0]
+
+	ind_non_zero = np.where(pitchData[start_ind1:end_ind1,1]>-10000)[0]
+	max_pitch_range = np.max(pitchData[start_ind1:end_ind1,1][ind_non_zero])+100
+	min_pitch_range = np.min(pitchData[start_ind1:end_ind1,1][ind_non_zero])-100
+
+	fsize = 18
+	fsize2 = 16
+
+	fig = plt.figure() 
+	ax = fig.add_subplot(111)    
+
+	plt.plot(hop*np.arange(pitchData.shape[0])[:end_ind1-start_ind1], pitchData[start_ind1:end_ind1,1], 'k', linewidth=1)
+
+	plt.ylim([min_pitch_range, max_pitch_range])
+	plt.xlabel("Time (s)", fontsize = fsize)
+	plt.ylabel("Frequency (Cents)", fontsize = fsize, labelpad=fsize2)
+
+	xLim = ax.get_xlim()
+	yLim = ax.get_ylim()
+	print yLim
+
+	ax.set_aspect((xLim[1]-xLim[0])/(2*float(yLim[1]-yLim[0])))
+	plt.tick_params(axis='both', which='major', labelsize=fsize2)
+
+	if isinstance(plotName, int):
+		plt.show()				
+	elif isinstance(plotName, str):
+		fig.savefig(plotName, bbox_inches='tight')
+
+	return 1 		
